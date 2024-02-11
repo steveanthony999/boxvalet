5e403f9e8a978db6872972a67254539c67d3e5ea
Steven Woodward <steveanthony999@gmail.com>
2024-02-10 16:49:43 -0800

    Added title and description metadata, added privacy policy and terms of service routes

---
+++ src/app/privacypolicy/page.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <main>
+      <div>
+        <h1>Privacy Policy</h1>
+        <h3>Page Coming Soon...</h3>
+      </div>
+    </main>
+  );
+}
---
+++ src/app/termsofservice/page.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <main>
+      <div>
+        <h1>Terms of Service</h1>
+        <h3>Page Coming Soon...</h3>
+      </div>
+    </main>
+  );
+}
